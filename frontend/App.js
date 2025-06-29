e84a157f220784d15bcb87f97a5dfa820833b017
Apparao <apparaobalkunde901@gmail.com>
2025-06-29 17:47:01 +0530

    fix

--- frontend/App.js
+++ frontend/App.js
@@ -1,12 +1,1 @@
-import React from 'react';
-
-function App() {
-  return (
-    <div>
-      <h1>GharChaZayka - Home Food Products</h1>
-      <p>Welcome to your home-made food store!</p>
-    </div>
-  );
-}
-
-export default App;
+fetch('/api/products').then(res => res.json()).then(data => console.log(data));
